d17dc9297283de988f04355449262960b5778260
Javier Suarez <javiersuarez_09@hotmail.com>
2020-04-26 11:32:57 -0500

    version final

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -205,7 +205,7 @@
 						if(!empty($_POST['fotoActual']) && file_exists($_POST['fotoActual'])){
 							unlink($_POST['fotoActual']);
 						}else{
-							shell_exec("mkdir vistas/img/usuarios/".$_POST['EditarCorreo']."");
+							mkdir($directorio);
 						}
 												
 
