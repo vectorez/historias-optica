14ebd88824bed72b9c6a6e8ce0b475dba8e9d8a5
Javier Suarez <javiersuarez_09@hotmail.com>
2020-04-26 15:57:03 -0500

    version final

--- vistas/js/pacientes.js
+++ vistas/js/pacientes.js
@@ -226,7 +226,15 @@ $.fn.datepicker.dates['es'] = {
     weekStart: 0
 };
 
-$("#NuevoFechaNacPa").datepicker('option', {language: "es", minDate: new Date(1900,1-1,1), maxDate: '-1Y'});
+$("#NuevoFechaNacPa").datepicker({
+    language: "es",
+    autoclose: true,
+    defaultDate: new Date(1970,1-1,1),
+    changeMonth: true,
+    changeYear: true,
+    yearRange: '-110:-1',
+    minDate: new Date(1900,1-1,1), maxDate: '-1Y'
+});
 
 $("#EditarFechaNacPa").datepicker({
     language: "es",
